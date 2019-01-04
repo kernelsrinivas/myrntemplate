9ec3a8b60ba7c8b56e700551da37b7a4957fa70b
kernelsrinivas <kernel.srinivasan@gmail.com>
2019-01-04 14:06:43 +0530

    demo commit

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,11 +16,18 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
+class HomePage extends Component
+{
+    static navigationOptions = {
+      title: 'Home',
+    };
+
+    render()
+    {
+      const { navigate } = this.props.navigation;
+      return (
       <View style={styles.container}>
+        <Icon name="rocket" size={30} color="#900" />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -33,7 +37,44 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Button onPress={()=>navigate("Stack")} title="Next Page"></Button>
       </View>
+      );
+    }
+}
+
+class StackPage extends Component
+{  
+    static navigationOptions = {
+      title: 'Stack Navigation',
+    };
+    render()
+    {
+      const { goBack } = this.props.navigation;
+      return(
+        <View style={styles.container}>
+        <Button onPress={()=>goBack()} title="Back to Home"></Button>
+        </View>
+      );
+    }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+      Home: HomePage,
+      Stack: StackPage
+  },
+  {
+      initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return (
+        <AppContainer/>
     );
   }
 }
