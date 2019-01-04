38afc2de49e527f584fad3cfcdb94d553fa3d3c7
kernelsrinivas <kernel.srinivasan@gmail.com>
2019-01-04 14:19:17 +0530

    warning remove commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-
+import {YellowBox} from 'react-native';
+YellowBox.ignoreWarnings(['Warning: ...']);
+console.disableYellowBox = true;
 AppRegistry.registerComponent('MyRNTemplate', () => App);
